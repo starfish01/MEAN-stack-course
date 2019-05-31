1da8d9405878e0a9b2b69f8a8740fc0f14000a4d
Patrick Labes <patrick.labes@gmail.com>
2019-05-31 15:06:07 +1000

    file upload implementation

--- Section 6 - Adding Image Uploads to our App/course/src/app/posts/post-create/post-create.component.ts
+++ Section 6 - Adding Image Uploads to our App/course/src/app/posts/post-create/post-create.component.ts
@@ -18,7 +18,8 @@ export class PostCreateComponent implements OnInit {
   private postId: string;
   post: Post;
   isLoading = false;
-  form: FormGroup
+  form: FormGroup;
+  imagePreview:string;
 
 
   constructor(public postsService: PostsService, public route:ActivatedRoute) { }
@@ -27,6 +28,7 @@ export class PostCreateComponent implements OnInit {
     this.form = new FormGroup({
       'title': new FormControl(null,{validators: [Validators.required, Validators.minLength(3)]}),
       'content': new FormControl(null,{validators: [Validators.required, Validators.minLength(5)]}),
+      'image': new FormControl(null,{validators:[Validators.required]})
     });
 
      this.route.paramMap.subscribe((paramMap:ParamMap)=>{
@@ -48,6 +50,17 @@ export class PostCreateComponent implements OnInit {
         this.postId = null;
       }
      });
+  }
+
+  onImagePicked(event:Event) {
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({image:file});
+    this.form.get('image').updateValueAndValidity();
+    const reader = new FileReader();
+    reader.onload = ()=>{
+      this.imagePreview = reader.result as string;
+    }
+    reader.readAsDataURL(file);
   }
 
 
